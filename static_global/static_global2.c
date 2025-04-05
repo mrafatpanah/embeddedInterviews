7e1e25bc8d239ab6a9ce7ef35231831ae412f1e1
Mohammad <m.rafatpanah@easygigs.ir>
2025-04-05 13:47:53 +0330

    commenting the results on code

--- static_global/static_global2.c
+++ static_global/static_global2.c
@@ -9,12 +9,13 @@ int main(void) {
     printf("Starting program...\n");
     
     // Call public functions from static_global1.c
-    public_increment();
-    public_increment();
-    get_status();
-    public_increment();
-    get_status();
-    printf("ModuleCounter status in 2nd file with the exact same name: %d\n", moduleCounter);  // Error: static variable not visible here
+    public_increment();     // Counter incremented to: 1
+    public_increment();     // Counter incremented to: 2
+    get_status();           // Current module status: 2 operations performed
+    public_increment();     // Counter incremented to: 3
+    get_status();           // Current module status: 3 operations performed
+    printf("ModuleCounter status in 2nd file with" // moduleCounter = 0
+            "the exact same name: %d\n", moduleCounter); 
     
     // The following would cause compilation errors if uncommented:
     // static_increment();  // Error: static function not visible here
